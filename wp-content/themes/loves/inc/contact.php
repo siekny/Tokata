604eea3d9e365a38f5a58ca3963b93d1bea9bc10
Hor Seikny <seikny.planb@gmail.com>
2018-12-20 08:27:47 +0700

    form comment

--- wp-content/themes/loves/inc/contact.php
+++ wp-content/themes/loves/inc/contact.php
@@ -4,12 +4,25 @@
 <div class="container" style="padding: 0">
 	<div class="gallery">
 	    <hr class="border-gallery">
-		<h1>Contact</h1>
-		<div class="content">
-			<div class="content-box">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui gravida tellus lobortis ullamcorper. Sed sit amet interdum magna. Nulla vulputate mollis lectus, sit amet porttitor massa ultricies volutpat.</p>
-			</div>
+		<h1>Comment</h1>
+
+		<?php 
+			$args = array(
+				'post_type'			=>			'project',
+				'post_per_page'		=>			1,
+				'category_name'		=>			'contact',
+			);
+			$query = new WP_Query($args);
+		?>
+		
+		<?php while($query->have_posts()) : $query->the_post(); ?>
+		<div class="content content-comment">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon.jpg">
+			<!-- <textarea class="form-control" placeholder="Comment Here" rows="5"></textarea>
+			<input type="submit" value="Comment" class="comment"> -->
+			<?php the_content(); ?>
 		</div>
+	<?php endwhile; ?>
 	</div>
 </div>
 
